da8f450a0221a2db4e38a8681c7bd818e5872771
Umar Saleem <105920914+umarsaleem62350@users.noreply.github.com>
2023-04-12 18:22:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Application;
+using Contracts;
+using JsonDataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserService, UserServiceImpl>();
+builder.Services.AddScoped<IForumService, ForumServiceImpl>();
+
+/*builder.Services.AddDbContext<ForumContext>();
+builder.Services.AddScoped<IUserDAO, UserSQLDAO>();
+builder.Services.AddScoped<IForumDAO, ForumSQLDAO>();*/
+builder.Services.AddScoped<JsonForumContext>();
+builder.Services.AddScoped<JsonUserContext>();
+builder.Services.AddScoped<IForumDAO, ForumDAOImpl>();
+builder.Services.AddScoped<IUserDAO, UserDAOImpl>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
